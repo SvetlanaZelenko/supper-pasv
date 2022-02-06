38bf5f4f0a97f8aff093b01c335254dc84b282d4
Svetlana Zelenko <sveta.g.zelenko@gmail.com>
2022-02-05 22:20:14 -0800

    add environment variable support

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,12 +1,14 @@
 import supertest from 'supertest'
-import { expect} from 'chai'
+import { expect } from 'chai'
+import 'dotenv/config'
 
 describe('Auth', function () {
-    const request = supertest('http://paysis.herokuapp.com')
+    const request = supertest('process.env.BASE_URL')
+
     it('Succesful log in', function () {
         request
             .post('/auth')
-            .send({login: 'adminius', password: 'supers3cret'})
+            .send({login: process.env.LOGIN, password: process.env.PASSWORD})
             .end(function (err, res) {
                 expect(res.statusCode).to.eq(200)
                 expect(res.body.token).not.to.be.undefined
