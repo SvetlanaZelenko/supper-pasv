2ad3a7ef2197a9ddd833b1c8b0a0133f35be4cef
Svetlana Zelenko <sveta.g.zelenko@gmail.com>
2022-02-14 10:18:40 -0800

    updated file auth.spec

--- helpers/auth-helper.js
+++ helpers/auth-helper.js
@@ -3,22 +3,14 @@ import supertest from 'supertest'
 class AuthHelper {
     response
 
-  async login(username, password)  {
-    await supertest(process.env.BASE_URL)
-        .post('/auth')
-<<<<<<< HEAD
-<<<<<<< HEAD
-        .send({ login: username, password: password })
-=======
-        .send({ login: 'username', password: 'password' })
->>>>>>> 114253f (add file auth-helper (#6))
-=======
-        .send({ login: 'username', password: 'password' })
->>>>>>> origin/main
-        .then(res => {
-          this.response = res
-        })
-  }
+    async login(username, password)  {
+        await supertest(process.env.BASE_URL)
+            .post('/auth')
+            .send({ login: username, password: password })
+            .then(res => {
+                this.response = res
+            })
+    }
 }
 
 export default AuthHelper
