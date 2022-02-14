f67a5c369f6c5d30c342840c63269bac6272ebbb
SvetlanaZelenko <94322131+SvetlanaZelenko@users.noreply.github.com>
2022-02-13 19:09:23 -0800

    add file auth-helper (#6)

--- helpers/auth-helper.js
+++ helpers/auth-helper.js
@@ -6,7 +6,11 @@ class AuthHelper {
   async login(username, password)  {
     await supertest(process.env.BASE_URL)
         .post('/auth')
+<<<<<<< HEAD
         .send({ login: username, password: password })
+=======
+        .send({ login: 'username', password: 'password' })
+>>>>>>> 114253f (add file auth-helper (#6))
         .then(res => {
           this.response = res
         })
